839ebba7ea1d5d72183d627ada89930a62bc9baf
Hitesh Kumar Saini <57103779+hit7sh@users.noreply.github.com>
2020-07-19 20:18:51 +0530

    Update CACHEHIT.cpp

--- CACHEHIT.cpp
+++ CACHEHIT.cpp
@@ -2,6 +2,7 @@
 #include<bits/stdc++.h>
 
 #define __speed() ios_base::sync_with_stdio(false), cin.tie(0), cout.tie(0);
+#define tt int t;for(cin >> t; t--; )
 #define findmax(v) *max_element(v.begin(), v.end())
 #define f0(i, n) for(i = 0; i < (int)(n); i++)
 #define endl "\n"
